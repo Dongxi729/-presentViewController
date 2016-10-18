c3775e0939b3cad962a62e4f7aaa81a6f10cc32a
zdx <18259129536@163.com>
2016-10-18 22:46:52 +0800

    Initial Commit

---
+++ jhjjjj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jhjjjj
+//
+//  Created by 郑东喜 on 16/10/18.
+//  Copyright © 2016年 郑东喜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jhjjjj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jhjjjj
+//
+//  Created by 郑东喜 on 16/10/18.
+//  Copyright © 2016年 郑东喜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
